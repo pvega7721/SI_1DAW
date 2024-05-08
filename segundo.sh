61c7c3fa2005a1aabdda1a3304cc7b46204edd87
Pablo Vega <pvega7721@ceuandalucia.es>
2024-05-08 15:55:12 +0200

    Primer commit

---
+++ segundo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo esto es un asterisco * sin comillas
+echo esto es un dólar y tres letras $ABC sin comillas
+echo "esto es un asterisco * entre comillas dobles"
+echo 'esto es un asterisco * entre comillas simples'
+echo "esto es un dólar y tres letras $ACB entre comillas dobles"
+echo 'esto es un dólar y tres letras $ABC entre comillas simples'
---
+++ tercero.sh
@@ -0,0 +1,3 @@
+echo esto \
+es una sola línea \
+aunque ocupe 3 en pantalla. # esto es una línea escrito en tres.
